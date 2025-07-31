d668108d18eb1058b52c78fa442bc09f2fc03cdd
Cristiane Sartori <sartori.cris@gmail.com>
2025-07-31 20:16:00 -0300

    Product-OrderItem one-to-many association Payment, Subtotal & Total methods

--- src/main/java/com/educandoweb/course/entities/enums/OrderStatus.java
+++ src/main/java/com/educandoweb/course/entities/enums/OrderStatus.java
@@ -1,19 +1,19 @@
 package com.educandoweb.course.entities.enums;
 
 public enum OrderStatus {
-	
-	WAITTING_PAYMENT(1),
+
+	WAITING_PAYMENT(1),
 	PAID(2),
 	SHIPPED(3),
 	DELIVERED(4),
 	CANCELED(5);
-	
+
 	private int code;
-	
+
 	private OrderStatus(int code) {
 		this.code = code;
 	}
-	
+
 	public int getCode() {
 		return code;
 	}
